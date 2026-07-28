3589986bfb110216395ac618de7e513b645607d1
techadnank9 <techadnank9@gmail.com>
2026-07-27 18:18:31 -0700

    Move Google Maps API key to env var, remove hardcoded secret

--- src/Map.js
+++ src/Map.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Map = ({ selectedCity, path ,startingCity}) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=REDACTED&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
     script.async = true;
     document.head.appendChild(script);
 
